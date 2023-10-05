4762cb5dd0db3bdc52eac398e1bdb44cfe5c3f7f
DeivinsonPereira <lordpoxtaa@gmail.com>
2023-10-05 08:48:38 -0300

    file .exe created

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     converter = PDFToDOCXConverter(root)
     converter.pack()
+    root.iconbitmap("icon.ico")  # Substitua "icon.ico" pelo nome do seu arquivo de ícone
+    root.title("AutoDocWizard")
 
     root.mainloop()
